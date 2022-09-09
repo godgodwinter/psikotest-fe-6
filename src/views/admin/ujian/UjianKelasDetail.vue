6bbc4cec57ade345242cd029fcad48591af2b850
Kukuh Setya Nugraha <kakadz.prou@gmail.com>
2022-09-09 17:44:05 +0700

    update reset waktu and jawaban

--- src/views/admin/ujian/UjianKelasDetail.vue
+++ src/views/admin/ujian/UjianKelasDetail.vue
@@ -44,7 +44,7 @@ const columns = [
     type: "String",
   },
   {
-    label: "Jumlah Siswa",
+    label: "Jumlah Kategori Soal",
     field: "siswa_jml",
     type: "String",
   },
@@ -149,6 +149,14 @@ const doDeleteData = async (id, index) => {
 
                   </button>
                 </router-link> -->
+
+                <button class="btn btn-sm btn-warning tooltip" data-tip="Export Data KELAS">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                  </svg>
+                </button>
               </div>
             </template>
             <template #table-row="props">
@@ -172,6 +180,14 @@ const doDeleteData = async (id, index) => {
                         clip-rule="evenodd" />
                     </svg>
                   </button> -->
+
+                  <button class="btn btn-sm btn-warning tooltip" data-tip="Export Data SISWA">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                    </svg>
+                  </button>
                 </div>
               </span>
 
--- src/views/admin/ujian/UjianKelasDetailSiswa.vue
+++ src/views/admin/ujian/UjianKelasDetailSiswa.vue
@@ -114,11 +114,39 @@ const doDeleteData = async (id, index) => {
     }
   }
 };
+
+const doResetWaktu = async (kelas_siswa_id) => {
+  if (confirm("Apakah anda mereset waktu data ini?")) {
+    // console.log(kelas_siswa_id);
+    try {
+      const response = await Api.post(`admin/menuujian/proesssiswa/${kelas_siswa_id}/reset_waktu`);
+      console.log(response);
+      Toast.babeng('Reset waktu berhasil!',)
+      // return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+const doResetAll = async (kelas_siswa_id) => {
+  if (confirm("Apakah anda mereset waktu dan jawaban data ini?")) {
+    // console.log(kelas_siswa_id);
+    try {
+      const response = await Api.post(`admin/menuujian/proesssiswa/${kelas_siswa_id}/reset_all`);
+      console.log(response);
+      Toast.babeng('Hard Reset berhasil!',)
+      // return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
 </script>
-  <template>
+<template>
   <h1 class="text-lg font-bold">SEKOLAH : {{ dataProses.nama }}</h1>
   <h1 class="text-lg font-bold">KELAS : {{ proses_kelas_id }}</h1>
-  <h1 class="text-lg font-bold">Berisi Kategori per SiSWA </h1>
+  <!-- <h1 class="text-lg font-bold">Berisi Kategori per SiSWA </h1> -->
   <div class="md:py-2 px-4 lg:flex flex-wrap gap-4">
     <div class="w-full lg:w-full">
       <div class="bg-white shadow rounded-lg px-4 py-4">
@@ -126,9 +154,9 @@ const doDeleteData = async (id, index) => {
           <vue-good-table :line-numbers="true" :columns="columns" :rows="data" :search-options="{
             enabled: true,
           }" :pagination-options="{
-  enabled: true,
-  perPageDropdown: [10, 20, 50],
-}" styleClass="vgt-table striped bordered condensed" class="py-0">
+            enabled: true,
+            perPageDropdown: [10, 20, 50],
+          }" styleClass="vgt-table striped bordered condensed" class="py-0">
             <template #table-actions>
               <div class="space-x-1 space-y-1 gap-1">
                 <button class="btn btn-sm btn-secondary tooltip" data-tip="Refresh Data" @click="getData()">
@@ -136,6 +164,14 @@ const doDeleteData = async (id, index) => {
                     <path fill-rule="evenodd"
                       d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
                       clip-rule="evenodd" />
+                  </svg>
+                </button>
+
+                <button class="btn btn-sm btn-warning tooltip" data-tip="Export Data SISWA">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                   </svg>
                 </button>
                 <!-- <router-link :to="{
@@ -160,13 +196,13 @@ const doDeleteData = async (id, index) => {
                         params: { id: props.row.id },
                       }"
                     > -->
-                  <button class="btn btn-sm btn-primary tooltip" data-tip="Reset Data">
+                  <button class="btn btn-sm btn-primary tooltip" data-tip="Reset Waktu"
+                    @click="doResetWaktu(props.row.id)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                       stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round"
                         d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
                     </svg>
-
                   </button>
                   <!-- </router-link> -->
                   <!-- <button class="btn btn-sm btn-danger" @click="doDeleteData(props.row.id, props.index)">
@@ -176,6 +212,14 @@ const doDeleteData = async (id, index) => {
                           clip-rule="evenodd" />
                       </svg>
                     </button> -->
+
+                  <button class="btn btn-sm btn-warning tooltip" data-tip="Reset All" @click="doResetAll(props.row.id)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>
+                  </button>
                 </div>
               </span>
 
