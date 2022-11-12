03bce0a735b47fcd74433d5bb5330c3d8718d0d8
Kukuh Setya Nugraha <kakadz.pro@gmail.com>
2022-11-12 12:50:45 +0700

    tampilkan nilai per siswa

--- src/views/admin/sekolah/submenu/hasil_ujian/Persiswa.vue
+++ src/views/admin/sekolah/submenu/hasil_ujian/Persiswa.vue
@@ -72,16 +72,89 @@ const getDataUjian = async () => {
     }
 };
 getDataUjian();
+
+const sort = (items) => {
+    return items.sort((a, b) => {
+        return a.nilaiku < b.nilaiku ? 1 : -1;
+    });
+}
 </script>
 <template>
 
     <button class="btn btn-sm btn-info" @click="doBackToKelas()">Kembali</button>
 
-    nama:{{ dataSiswa.nama }}
+    <div class="md:py-2 px-4 lg:flex flex-wrap gap-4">
+        <div class="w-full lg:w-full">
+            <div class="bg-white shadow rounded-lg px-4 py-4">
+                <div class="overflow-x-auto">
+                    <table class="table table-compact">
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <td class="whitespace-nowrap w-1/12">No Induk</td>
+                                <td class="whitespace-nowrap w-1/12">:</td>
+                                <td class="whitespace-nowrap w-10/12">
+                                    {{ dataSiswa.nomeridentitas }}
+                                </td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <td>Nama</td>
+                                <td>:</td>
+                                <td>{{ dataSiswa.nama }}</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <td>Umur</td>
+                                <td>:</td>
+                                <td>{{ dataSiswa.umur }}</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <td>Jenis Kelamin</td>
+                                <td>:</td>
+                                <td>{{ dataSiswa.jk }}</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <td>Sekolah</td>
+                                <td>:</td>
+                                <td>{{ dataSiswa.sekolah_nama }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <ul>
+    <div class="md:py-2 px-4 lg:flex flex-wrap gap-4" v-for="item, index in data" :key="item.id">
+        {{ index + 1 }}. {{ item.nama }}
+        <div class="w-full lg:w-full">
+            <div class="bg-white shadow rounded-lg px-4 py-4">
+                <div class="overflow-x-auto">
+                    <table class="table table-compact">
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr v-for="mapel, i in sort(item.detail) " :key="mapel.id">
+                                <td class="whitespace-nowrap w-1/12">{{ i + 1 }}</td>
+                                <td class="whitespace-nowrap w-1/12">
+                                    {{ mapel.ujian_paketsoal_kategori?.kategori?.nama }}
+                                    <!-- {{ mapel }} -->
+                                </td>
+                                <td class="whitespace-nowrap w-1/12">:</td>
+                                <td class="whitespace-nowrap w-1/12">{{ mapel.nilaiku }}</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <ul>
         <li v-for="item in data" :key="item.id">
             {{ item.nama }}
         </li>
-    </ul>
+    </ul> -->
 </template>
