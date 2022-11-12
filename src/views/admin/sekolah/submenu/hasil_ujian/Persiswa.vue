c2793177842febfaa6309bd8e98bdabeb4fd3801
Kukuh Setya Nugraha <kakadz.pro@gmail.com>
2022-11-12 10:48:05 +0700

    add base template tampil hasil ujian per siswa

--- src/views/admin/sekolah/submenu/hasil_ujian/Persiswa.vue
+++ src/views/admin/sekolah/submenu/hasil_ujian/Persiswa.vue
@@ -19,8 +19,69 @@ import { useStoreAdminBar } from "@/stores/adminBar";
 import { useRouter, useRoute } from "vue-router";
 import Toast from "@/components/lib/Toast.js";
 
+const router = useRouter();
+const route = useRoute();
+
+
+const id = route.params.id;
+const kelas_id = ref(route.params.kelas_id ? route.params.kelas_id : 0);
+const siswa_id = route.params.siswa_id;
+const dataAsli = ref([]);
+const dataSiswa = ref([]);
+const data = ref([]);
+const dataKelas = ref([]);
+
+const doBackToKelas = () => {
+    // console.log(inputCariKelas.value.id);
+    router.push({
+        name: "AdminSekolahDetail.hasil_ujian",
+        params: {
+            id: id,
+            kelas_id: kelas_id.value,
+        },
+    });
+};
+
+
+const getDataSiswa = async () => {
+    try {
+        data.value = [];
+        const response = await Api.get(
+            `owner/datasekolah/${id}/siswa/${siswa_id}}`
+        );
+        dataSiswa.value = response.data;
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+getDataSiswa();
+const getDataUjian = async () => {
+    try {
+        dataAsli.value = [];
+        data.value = [];
+        const response = await Api.get(
+            `admin/menuujian/hasil_ujian/${siswa_id}`
+        );
+        dataAsli.value = response.data;
+        data.value = dataAsli.value;
+        console.log(data.value);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+getDataUjian();
 </script>
 <template>
 
-    Persiswa
+    <button class="btn btn-sm btn-info" @click="doBackToKelas()">Kembali</button>
+
+    nama:{{ dataSiswa.nama }}
+
+    <ul>
+        <li v-for="item in data" :key="item.id">
+            {{ item.nama }}
+        </li>
+    </ul>
 </template>
