ede414e8508265935be1ef4c516c704acaa8c64c
Kukuh Setya Nugraha <kakadz.prou@gmail.com>
2022-12-01 09:25:45 +0700

    add cetak perkelas

--- src/views/admin/sekolah/submenu/hasil_ujian/Index.vue
+++ src/views/admin/sekolah/submenu/hasil_ujian/Index.vue
@@ -154,6 +154,11 @@ const columns = ref([
     field: "ujian_siswa",
     type: "String",
   },
+  {
+    label: "Status",
+    field: "selesai",
+    type: "String",
+  },
 ]);
 
 
@@ -222,8 +227,20 @@ const doCetak = (kelas_id = null, token = moment().format("YYYY-MM-DD")) => {
                 <router-link :to="{
                   name: 'AdminSekolahDetail.hasil_ujian.persiswa',
                   params: { id, kelas_id, siswa_id: props.row.id },
-                }" v-if="props.row.ujian_siswa">
+                }" v-if="props.row.selesai == 'Selesai'">
                   <button data-tip="DATA UJIAN" class="tooltip btn btn-success btn-sm text-base-content">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                      stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                    </svg>
+                  </button>
+                </router-link>
+                <router-link :to="{
+                  name: 'AdminSekolahDetail.hasil_ujian.persiswa',
+                  params: { id, kelas_id, siswa_id: props.row.id },
+                }" v-else-if="props.row.selesai == 'Belum diselesaikan'">
+                  <button data-tip="DATA UJIAN" class="tooltip btn btn-warning btn-sm text-base-content">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                       stroke="currentColor" stroke-width="2">
                       <path stroke-linecap="round" stroke-linejoin="round"
