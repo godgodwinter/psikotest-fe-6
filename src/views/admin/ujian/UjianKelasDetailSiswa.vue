9c007939a0cd76f58d7dd54677093b920a73dc57
Kukuh Setya Nugraha <kakadz.prou@gmail.com>
2022-12-24 11:17:31 +0700

    update layout

--- src/views/admin/ujian/UjianKelasDetailSiswa.vue
+++ src/views/admin/ujian/UjianKelasDetailSiswa.vue
@@ -333,13 +333,13 @@ const onSubmit = async (values) => {
                     </svg>
                   </button>
 
-                  <button class="btn btn-sm btn-warning tooltip" data-tip="Reset All" @click="doResetAll(props.row.id)">
+                  <!-- <button class="btn btn-sm btn-warning tooltip" data-tip="Reset All" @click="doResetAll(props.row.id)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                       stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round"
                         d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                     </svg>
-                  </button>
+                  </button> -->
                   <button class="btn btn-sm btn-danger tooltip" data-tip="Ubah Nilai"
                     @click="doEditForm(props.row.ujian_paketsoal_kategori_id, props.row.nama, props.row.nilaiAkhir_revisi)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
