ecece3a5a7a0825e773cd606a993c714ead7a3f5
Kukuh Setya Nugraha <kakadz.prou@gmail.com>
2022-12-24 08:36:11 +0700

    add menu reset jawaban salah

--- src/views/admin/ujian/UjianKelasDetailSiswa.vue
+++ src/views/admin/ujian/UjianKelasDetailSiswa.vue
@@ -144,6 +144,19 @@ const doResetWaktu = async (kelas_siswa_id) => {
     }
   }
 }
+const doResetSalah = async (kelas_siswa_id) => {
+  if (confirm("Apakah anda mereset waktu data ini?")) {
+    // console.log(kelas_siswa_id);
+    try {
+      const response = await Api.post(`admin/menuujian/proesssiswa/${kelas_siswa_id}/reset_salah`);
+      console.log(response);
+      Toast.babeng('Reset waktu dan Jawaban Salah berhasil!',)
+      // return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
 const doResetAll = async (kelas_siswa_id) => {
   if (confirm("Apakah anda mereset waktu dan jawaban data ini?")) {
     // console.log(kelas_siswa_id);
@@ -311,6 +324,14 @@ const onSubmit = async (values) => {
                           clip-rule="evenodd" />
                       </svg>
                     </button> -->
+                  <button class="btn btn-sm btn-success tooltip" data-tip="Reset Jawaban Salah"
+                    @click="doResetSalah(props.row.id)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                    </svg>
+                  </button>
 
                   <button class="btn btn-sm btn-warning tooltip" data-tip="Reset All" @click="doResetAll(props.row.id)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
