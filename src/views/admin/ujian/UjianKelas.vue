8d20f8bd5df9ee9a1112e5128f7da7bac162bed0
godgodwinter <kakadz.pro@gmail.com>
2023-02-05 19:51:59 +0700

    update

--- src/views/admin/ujian/UjianKelas.vue
+++ src/views/admin/ujian/UjianKelas.vue
@@ -127,6 +127,23 @@ const doGeneratePerkelas = async (kelas_id = kelas_id.value) => {
     }
   };
 };
+
+
+const doGenerateSoaldanRandomPilihanJawaban = async (id) => {
+  if (confirm("Apakah anda yakin generate Soal dan PilihanJawaban data ini?")) {
+    try {
+      console.log(id);
+      // const response = await Api.get(
+      //   `admin/hasil_ujian_lintas/kelas/${kelas_id}`
+      // );
+      Toast.success("Info", "Proses Generate Soal dan pilihan berhasil!")
+      return response.data;
+    } catch (error) {
+      Toast.danger("Warning", "Proses Generate Soal dan pilihan gagal!")
+      console.error(error);
+    }
+  };
+};
 </script>
 <template>
   <h1 class="text-lg font-bold">DETAIL : {{ dataProses.nama }}</h1>
@@ -185,6 +202,17 @@ const doGeneratePerkelas = async (kelas_id = kelas_id.value) => {
                     </svg>
 
                   </button>
+                  <!-- <button @click="doGenerateSoaldanRandomPilihanJawaban(props.row.id)"
+                    class="btn btn-warning btn-sm tooltip" data-tip="generate Soal dan Pilihan Jawaban">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
+                    </svg>
+
+                  </button> -->
                   <!-- <button class="btn btn-sm btn-danger" @click="doDeleteData(props.row.id, props.index)">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd"
