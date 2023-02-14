783682ac80028675724166333e4756811e2b09f6
Bagus Candra <bacanprasetyo@gmail.com>
2023-02-14 21:42:57 +0700

    update keterangan lintas studi

--- src/components/lib/Psikotest.js
+++ src/components/lib/Psikotest.js
@@ -128,52 +128,48 @@ const getKesimpulanEqSq = (item = null) => {
 };
 
 export const fn_studi_ket = (item = null) => {
-  let hasil = "Moron";
-  if (item > 139) {
-    hasil = "Genius";
-  } else if (140 > item && item >= 130) {
-    hasil = "Berbakat";
-  } else if (130 > item && item >= 120) {
-    hasil = "Superior";
-  } else if (120 > item && item >= 110) {
-    hasil = "Di Atas Rata - Rata";
-  } else if (110 > item && item >= 105) {
-    hasil = "Rata - Rata Atas";
-  } else if (105 > item && item >= 100) {
-    hasil = "Rata - Rata";
-  } else if (100 > item && item >= 90) {
-    hasil = "Rata - Rata Bawah";
-  } else if (90 > item && item >= 80) {
-    hasil = "Lambat Belajar";
-  } else if (80 > item && item >= 60) {
-    hasil = "Borderline";
+  let hasil = "Angka tidak valid!";
+  if (item > 88) {
+    hasil = "Sangat Baik Sekali";
+  } else if (88 > item && item >= 78) {
+    hasil = "Baik Sekali";
+  } else if (78 > item && item >= 68) {
+    hasil = "Baik";
+  } else if (68 > item && item >= 61) {
+    hasil = "Cukup Baik";
+  } else if (61 > item && item >= 44) {
+    hasil = "Cukup";
+  } else if (44 > item && item >= 34) {
+    hasil = "Hampir Cukup";
+  } else if (34 > item && item >= 28) {
+    hasil = "Kurang";
+  } else if (28 > item && item >= 18) {
+    hasil = "Kurang Sekali";
   } else {
-    hasil = "Moron";
+    hasil = "Sangat Kurang Sekali";
   }
   return hasil;
 };
 export const fn_studi_ket_singkatan = (item = null) => {
-  let hasil = "Moron";
-  if (item > 139) {
-    hasil = "Genius";
-  } else if (140 > item && item >= 130) {
-    hasil = "Berbakat";
-  } else if (130 > item && item >= 120) {
-    hasil = "Superior";
-  } else if (120 > item && item >= 110) {
-    hasil = "Di Atas Rata - Rata";
-  } else if (110 > item && item >= 105) {
-    hasil = "Rata - Rata Atas";
-  } else if (105 > item && item >= 100) {
-    hasil = "Rata - Rata";
-  } else if (100 > item && item >= 90) {
-    hasil = "Rata - Rata Bawah";
-  } else if (90 > item && item >= 80) {
-    hasil = "Lambat Belajar";
-  } else if (80 > item && item >= 60) {
-    hasil = "Borderline";
+  let hasil = "Angka tidak valid!";
+  if (item > 88) {
+    hasil = "SBS";
+  } else if (88 > item && item >= 78) {
+    hasil = "BS";
+  } else if (78 > item && item >= 68) {
+    hasil = "B";
+  } else if (68 > item && item >= 61) {
+    hasil = "CB";
+  } else if (61 > item && item >= 44) {
+    hasil = "C";
+  } else if (44 > item && item >= 34) {
+    hasil = "HC";
+  } else if (34 > item && item >= 28) {
+    hasil = "K";
+  } else if (28 > item && item >= 18) {
+    hasil = "KS";
   } else {
-    hasil = "Moron";
+    hasil = "SKS";
   }
   return hasil;
 };
