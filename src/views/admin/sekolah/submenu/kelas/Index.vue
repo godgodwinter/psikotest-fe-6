59290b7f3bf82306a10c74a42bfa4996d7c00bb1
godgodwinter <kakadz.pro@gmail.com>
2023-05-08 23:34:27 +0700

    add cetak deteksi with ts

--- src/views/admin/sekolah/submenu/kelas/Index.vue
+++ src/views/admin/sekolah/submenu/kelas/Index.vue
@@ -1,4 +1,7 @@
 <script setup>
+import ButtonCetak from "@/components/atoms/ButtonCetak.vue";
+
+
 import Api from "@/axios/axios";
 import { ref, watch, computed } from "vue";
 import BreadCrumb from "@/components/atoms/BreadCrumb.vue";
@@ -108,6 +111,17 @@ const forceDestroy = async (id2, index) => {
     }
   }
 };
+
+
+const doCetakDeteksi = (id = null, token = moment().format("YYYY-MM-DD")) => {
+  if (id === null) {
+    Toast.danger("Warning", "Data tidak valid!");
+  } else {
+    window.open(
+      `${BASE_URL}api/guest/cetak/deteksimasalah_perkelas/${id}`
+    );
+  }
+};
 </script>
 <template>
   <div class="md:py-2 px-4 lg:flex flex-wrap gap-4">
@@ -157,6 +171,15 @@ const forceDestroy = async (id2, index) => {
                   </router-link>
                   <button class="btn btn-sm btn-primary tooltip" data-tip="Cetak Per kelas "
                     @click="doExport(props.row.id)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                    </svg>
+
+                  </button>
+                  <button class="btn btn-sm btn-warning tooltip" data-tip="Cetak Deteksi Per kelas "
+                    @click="doCetakDeteksi(props.row.id)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                       stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round"
