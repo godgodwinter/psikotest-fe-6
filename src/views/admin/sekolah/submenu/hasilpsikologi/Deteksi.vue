6555baae1b94965cf7af46cdf6d2aea356d5d95f
godgodwinter <kakadz.pro@gmail.com>
2023-05-10 14:33:39 +0700

    add cetak tanpa ttd

--- src/views/admin/sekolah/submenu/hasilpsikologi/Deteksi.vue
+++ src/views/admin/sekolah/submenu/hasilpsikologi/Deteksi.vue
@@ -154,8 +154,25 @@ const doCetak_tanpa_ttd = (id = null, token = moment().format("YYYY-MM-DD")) => 
     <div>
       <span class="text-2xl sm:text-3xl leading-none font-bold text-gray-700 shadow-sm">Deteksi Psikologis</span>
 
-      <ButtonCetak @click="doCetak(id2)" />
-      <ButtonCetak @click="doCetak_tanpa_ttd(id2)" />
+      <!-- <ButtonCetak @click="doCetak(id2)" />
+      <ButtonCetak @click="doCetak_tanpa_ttd(id2)" /> -->
+      <button class="btn btn-sm btn-warning tooltip" data-tip="Cetak Deteksi Masalah " @click="doCetak(id2)">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+        </svg>
+
+
+      </button>
+      <button class="btn btn-sm btn-info tooltip" data-tip="Cetak Deteksi Masalah tanpa ttd"
+        @click="doCetak_tanpa_ttd(id2)">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+        </svg>
+      </button>
     </div>
     <div class="md:py-0 py-4 space-x-2 space-y-2">
       <span @click="router.go(-1)">
